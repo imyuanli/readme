44cacb7a717cd655aaf0a98a26041bfa2428553f
yuanli <2865437316@qq.com>
2022-10-11 18:22:36 +0800

    markdown 数据结构

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
 import {Button} from "@mui/material";
 import Title from "@/components/title";
 import {useState} from "react";
-import {DEFAULT_DATA, DEFAULT_PREFIX} from "@/constants/default";
+import {DEFAULT_DATA, DEFAULT_PREFIX,DEFAULT_LINK} from "@/constants/default";
 import SubTitle from "@/components/subtitle";
 import Work from "@/components/work";
 import {generateMarkdown} from "@/utils/util";
@@ -9,10 +9,11 @@ import {generateMarkdown} from "@/utils/util";
 export default function HomePage() {
     const [prefix, setPrefix] = useState(DEFAULT_PREFIX)
     const [data, setData] = useState(DEFAULT_DATA)
+    const [link, setLink] = useState(DEFAULT_LINK)
     const [showMd, setShowMd] = useState(false)
     const [mdContent, setMdContent] = useState(false)
     const handlePrefixChange = (key: any, value: any) => {
-        const res = {...data}
+        const res = {...prefix}
         // @ts-ignore
         res[key] = value
         setPrefix(res)
@@ -23,10 +24,18 @@ export default function HomePage() {
         res[key] = value
         setData(res)
     }
+
+    const handleLinkChange = (key: any, value: any) => {
+        const res = {...link}
+        // @ts-ignore
+        res[key] = value
+        setLink(res)
+    }
+
     const handleGenerate = () => {
         setShowMd(true)
         // @ts-ignore
-        setMdContent(generateMarkdown(prefix,data))
+        setMdContent(generateMarkdown(prefix,data,link))
     }
     return (
         <div className='p-6'>
@@ -54,8 +63,11 @@ export default function HomePage() {
                         />
                         <Work prefix={prefix}
                               data={data}
+                              link={link}
                               handleDataChange={handleDataChange}
-                              handlePrefixChange={handlePrefixChange}/>
+                              handlePrefixChange={handlePrefixChange}
+                              handleLinkChange={handleLinkChange}
+                        />
                         <Button onClick={handleGenerate} variant="contained">生成</Button>
                     </>
             }
--- src/utils/util.ts
+++ src/utils/util.ts
@@ -1,8 +1,4 @@
-export function isValidKey(key: string | number | symbol, object: object): key is keyof typeof object {
-    return key in object;
-}
-
-export function generateMarkdown(prefix: any, data: any) {
+export function generateMarkdown(prefix: any, data: any,link:any) {
     return` 
 <h1 align="center">${prefix.title}，${data.title}</h1>
 <h3 align="center">${data.subtitle}</h3>
@@ -10,17 +6,9 @@ export function generateMarkdown(prefix: any, data: any) {
 <p align="left"> <a href="https://github.com/ryo-ma/github-profile-trophy"><img src="https://github-profile-trophy.vercel.app/?username=imyuanli" alt="imyuanli" /></a> </p>
 
 
-- ${prefix.currentWork} **${data.currentWork}**
-- ${prefix.currentLearn} **${data.currentWork}**
-- ${prefix.collaborateOn} **${data.currentWork}**
-<!-- - 🌱 I’m currently learning **https://github.com/rahuldkjain/github-profile-readme-generator**-->
-<!-- - 👯 I’m looking to collaborate on **[请问](请问)**-->
-<!-- - 🤝 I’m looking for help with [请问请问](请问)-->
-<!-- - 👨‍💻 All of my projects are available at [请问](请问)-->
-<!-- - 📝 I regularly write articles on [请问](请问)-->
-<!-- - 💬 Ask me about **请问**-->
-<!-- - 📫 How to reach me **请问**-->
-<!-- - 📄 Know about my experiences [请问](请问)-->
-<!-- - ⚡ Fun fact **请问**-->
+- ${prefix.currentWork} **[${data.currentWork}](${link.currentWork})**
+- ${prefix.currentLearn} **[${data.currentLearn}](${link.currentLearn})**
+- ${prefix.collaborateOn} **[${data.collaborateOn}](${link.collaborateOn})**
+
 `
 }
