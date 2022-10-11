b2cc9d88fff745df00d02d42636e4b9a3c4eb59b
yuanli <2865437316@qq.com>
2022-10-11 17:16:19 +0800

    生成 md文档

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,10 +4,13 @@ import {useState} from "react";
 import {DEFAULT_DATA, DEFAULT_PREFIX} from "@/constants/default";
 import SubTitle from "@/components/subtitle";
 import Work from "@/components/work";
+import {generateMarkdown} from "@/utils/util";
 
 export default function HomePage() {
     const [prefix, setPrefix] = useState(DEFAULT_PREFIX)
     const [data, setData] = useState(DEFAULT_DATA)
+    const [showMd, setShowMd] = useState(false)
+    const [mdContent, setMdContent] = useState(false)
     const handlePrefixChange = (key: any, value: any) => {
         const res = {...data}
         // @ts-ignore
@@ -20,27 +23,42 @@ export default function HomePage() {
         res[key] = value
         setData(res)
     }
-    const handleFinish = () => {
-        console.log(prefix)
-        console.log(data)
+    const handleGenerate = () => {
+        setShowMd(true)
+        // @ts-ignore
+        setMdContent(generateMarkdown(prefix,data))
     }
     return (
         <div className='p-6'>
-            <Title
-                prefix={prefix}
-                data={data}
-                handleDataChange={handleDataChange}
-                handlePrefixChange={handlePrefixChange}
-            />
-            <SubTitle
-                data={data}
-                handleDataChange={handleDataChange}
-            />
-            <Work  prefix={prefix}
-                   data={data}
-                   handleDataChange={handleDataChange}
-                   handlePrefixChange={handlePrefixChange} />
-            <Button onClick={handleFinish} variant="contained">生成</Button>
+            {
+                showMd ?
+                    <>
+                        <div className='whitespace-pre'>
+                            {mdContent}
+                        </div>
+                        <Button onClick={() => {
+                            setShowMd(false)
+                        }} variant="contained">取消</Button>
+                    </>
+                    :
+                    <>
+                        <Title
+                            prefix={prefix}
+                            data={data}
+                            handleDataChange={handleDataChange}
+                            handlePrefixChange={handlePrefixChange}
+                        />
+                        <SubTitle
+                            data={data}
+                            handleDataChange={handleDataChange}
+                        />
+                        <Work prefix={prefix}
+                              data={data}
+                              handleDataChange={handleDataChange}
+                              handlePrefixChange={handlePrefixChange}/>
+                        <Button onClick={handleGenerate} variant="contained">生成</Button>
+                    </>
+            }
         </div>
     );
 }
--- src/utils/util.ts
+++ src/utils/util.ts
@@ -1,3 +1,26 @@
-export function isValidKey(key: string | number | symbol , object: object): key is keyof typeof object {
+export function isValidKey(key: string | number | symbol, object: object): key is keyof typeof object {
     return key in object;
+}
+
+export function generateMarkdown(prefix: any, data: any) {
+    return` 
+<h1 align="center">${prefix.title}，${data.title}</h1>
+<h3 align="center">${data.subtitle}</h3>
+<p align="left"> <img src="https://komarev.com/ghpvc/?username=imyuanli&label=Profile%20views&color=0e75b6&style=flat" alt="imyuanli" /> </p>
+<p align="left"> <a href="https://github.com/ryo-ma/github-profile-trophy"><img src="https://github-profile-trophy.vercel.app/?username=imyuanli" alt="imyuanli" /></a> </p>
+
+
+- ${prefix.currentWork} **${data.currentWork}**
+- ${prefix.currentLearn} **${data.currentWork}**
+- ${prefix.collaborateOn} **${data.currentWork}**
+<!-- - 🌱 I’m currently learning **https://github.com/rahuldkjain/github-profile-readme-generator**-->
+<!-- - 👯 I’m looking to collaborate on **[请问](请问)**-->
+<!-- - 🤝 I’m looking for help with [请问请问](请问)-->
+<!-- - 👨‍💻 All of my projects are available at [请问](请问)-->
+<!-- - 📝 I regularly write articles on [请问](请问)-->
+<!-- - 💬 Ask me about **请问**-->
+<!-- - 📫 How to reach me **请问**-->
+<!-- - 📄 Know about my experiences [请问](请问)-->
+<!-- - ⚡ Fun fact **请问**-->
+`
 }
