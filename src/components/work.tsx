68b1cfd6c60cb5350247dfed15ab80665fac48f2
yuanli <2865437316@qq.com>
2022-10-11 18:20:11 +0800

    减少 冗余 并添加link

--- src/components/work.tsx
+++ src/components/work.tsx
@@ -2,54 +2,53 @@ import React from "react";
 import BaseTitle from "@/components/base/base-title";
 import PrefixInput from "@/components/base/prefix-input";
 import DataInput from "@/components/base/data-input";
+import LinkInput from "@/components/base/link-input";
 
 interface props {
     prefix: any,
     handleDataChange: any
     handlePrefixChange: any
     data: any
+    handleLinkChange: any,
+    link: any
 }
 
-const Work: React.FunctionComponent<props> = ({prefix, data, handleDataChange, handlePrefixChange}) => {
+const Work: React.FunctionComponent<props> = ({
+                                                  prefix,
+                                                  data,
+                                                  link,
+                                                  handleDataChange,
+                                                  handlePrefixChange,
+                                                  handleLinkChange
+                                              }) => {
+
+    const current = ['currentWork','currentLearn','collaborateOn']
     return (
         <div className='mb-8'>
-            <BaseTitle value={'Work'} />
-            <div className='flex mb-3'>
-                <PrefixInput
-                    handlePrefixChange={handlePrefixChange}
-                    value={prefix?.currentWork}
-                    type={'currentWork'}
-                />
-                <DataInput
-                    handleDataChange={handleDataChange}
-                    value={data?.currentWork}
-                    type={'currentWork'}
-                />
-            </div>
-            <div className='flex mb-3'>
-                <PrefixInput
-                    handlePrefixChange={handlePrefixChange}
-                    value={prefix?.currentLearn}
-                    type={'currentLearn'}
-                />
-                <DataInput
-                    handleDataChange={handleDataChange}
-                    value={data?.currentLearn}
-                    type={'currentLearn'}
-                />
-            </div>
-            <div className='flex mb-3'>
-                <PrefixInput
-                    handlePrefixChange={handlePrefixChange}
-                    value={prefix?.collaborateOn}
-                    type={'collaborateOn'}
-                />
-                <DataInput
-                    handleDataChange={handleDataChange}
-                    value={data?.collaborateOn}
-                    type={'collaborateOn'}
-                />
-            </div>
+            <BaseTitle value={'Work'}/>
+            {
+                current.map((item,index)=>{
+                    return(
+                        <div className='flex mb-3'>
+                            <PrefixInput
+                                handlePrefixChange={handlePrefixChange}
+                                value={prefix[item]}
+                                type={item}
+                            />
+                            <DataInput
+                                handleDataChange={handleDataChange}
+                                value={data[item]}
+                                type={item}
+                            />
+                            <LinkInput
+                                handleLinkChange={handleLinkChange}
+                                value={link[item]}
+                                type={item}
+                            />
+                        </div>
+                    )
+                })
+            }
         </div>
     )
 }
