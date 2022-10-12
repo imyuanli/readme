cb3b5976b5a30fb5aa1302cc3db79653ac06ba7b
yuanli <2865437316@qq.com>
2022-10-12 14:01:17 +0800

    渲染数据

--- src/components/work.tsx
+++ src/components/work.tsx
@@ -3,6 +3,7 @@ import BaseTitle from "@/components/base/base-title";
 import PrefixInput from "@/components/base/prefix-input";
 import DataInput from "@/components/base/data-input";
 import LinkInput from "@/components/base/link-input";
+import intl from "react-intl-universal";
 
 interface props {
     prefix: any,
@@ -21,30 +22,81 @@ const Work: React.FunctionComponent<props> = ({
                                                   handlePrefixChange,
                                                   handleLinkChange
                                               }) => {
-
-    const current = ['currentWork','currentLearn','collaborateOn']
+    const result = [
+        {
+            name: 'currentWork',
+            placeholderData: intl.get('projectName'),
+            placeholderLink: intl.get('projectLink'),
+        },
+        {
+            name: 'collaborateOn',
+            placeholderData: intl.get('projectName'),
+            placeholderLink: intl.get('projectLink'),
+        },
+        {
+            name: 'helpWith',
+            placeholderData: intl.get('projectName'),
+            placeholderLink: intl.get('projectLink'),
+        },
+        {
+            name: 'currentLearn',
+            placeholderData: intl.get('phLearn'),
+        },
+        {
+            name: 'ama',
+            placeholderData: intl.get('phAma'),
+        },
+        {
+            name: 'contact',
+            placeholderData: 'example@gmail.com',
+        },
+        {
+            name: 'portfolio',
+            placeholderLink: intl.get('phPortfolio'),
+        },
+        {
+            name: 'blog',
+            placeholderLink: intl.get('phBlog'),
+        },
+        {
+            name: 'resume',
+            placeholderLink: intl.get('phResume'),
+        },
+        {
+            name: 'funFact',
+            placeholderData: intl.get('phFunFact'),
+        },
+    ]
     return (
         <div className='mb-8'>
             <BaseTitle value={'Work'}/>
             {
-                current.map((item,index)=>{
-                    return(
-                        <div className='flex mb-3'>
+                result.map((item, index) => {
+                    return (
+                        <div className='flex mb-3' key={index}>
                             <PrefixInput
                                 handlePrefixChange={handlePrefixChange}
-                                value={prefix[item]}
+                                value={prefix[item?.name]}
                                 type={item}
                             />
-                            <DataInput
-                                handleDataChange={handleDataChange}
-                                value={data[item]}
-                                type={item}
-                            />
-                            <LinkInput
-                                handleLinkChange={handleLinkChange}
-                                value={link[item]}
-                                type={item}
-                            />
+                            {
+                                item?.placeholderData &&
+                                <DataInput
+                                    handleDataChange={handleDataChange}
+                                    value={data[item?.name]}
+                                    type={item}
+                                    placeholder={item?.placeholderData}
+                                />
+                            }
+                            {
+                                item?.placeholderLink &&
+                                <LinkInput
+                                    handleLinkChange={handleLinkChange}
+                                    value={link[item?.name]}
+                                    type={item}
+                                    placeholder={item?.placeholderLink}
+                                />
+                            }
                         </div>
                     )
                 })
