fe439584c6ffedd342346531cb5e6925c811c3a4
yuanli <2865437316@qq.com>
2023-03-27 14:41:02 +0800

    代码重构

--- src/layouts/index.tsx
+++ src/layouts/index.tsx
@@ -1,70 +1,60 @@
 import {Link, Outlet} from 'umi';
-// import styles from './index.less';
-import intl from 'react-intl-universal';
+// // import styles from './index.less';
+// import intl from 'react-intl-universal';
+// // import {useEffect, useState} from "react";
+// // import {MenuItem, Select} from "@mui/material";
+// // import {emit} from "@/locales/emit";
+// import en from '../locales/en-US'
+// import zh from '../locales/zh-CN'
 // import {useEffect, useState} from "react";
-// import {MenuItem, Select} from "@mui/material";
-// import {emit} from "@/locales/emit";
-import en from '../locales/en-US'
-import zh from '../locales/zh-CN'
-import {useEffect, useState} from "react";
-import {MenuItem, Select} from "@mui/material";
-
-const locales = {
-    "en": en,
-    "zh": zh,
-};
-let DEFAULT_PREFIX = {}
+//
+// const locales = {
+//     "en": en,
+//     "zh": zh,
+// };
+// let DEFAULT_PREFIX = {}
 export default function Layout() {
-    const [initDone, setInitDone] = useState(false)
-    useEffect(() => {
-        let lang = navigator.language.toLowerCase();
-        if (lang.indexOf('zh') >= 0) {
-            // 假如浏览器语言是中文
-            localStorage.setItem("defaultLng", "zh")
-        } else {
-            // 假如浏览器语言是其它语言
-            localStorage.setItem("defaultLng", "en")
-        }
-        loadLocales()
-    })
-    //默认
-    const  default_local = localStorage.getItem('locale') || localStorage.getItem("defaultLng") || 'zh'
-    const loadLocales = () => {
-        intl.init({
-            currentLocale: default_local,
-            locales,
-        }).then(() => {
-            DEFAULT_PREFIX = locales
-            setInitDone(true)
-        })
-    }
-
-    const [locale, setLocale] = useState(default_local)
-    //切换语言
-    const handleChange = (e: any) => {
-        let val = e.target.value
-        setLocale(val)
-        localStorage.setItem('locale', val)
-        window.location.reload()
-    }
-    const context = {
-        DEFAULT_PREFIX
-    }
+    // const [initDone, setInitDone] = useState(false)
+    // useEffect(() => {
+    //     let lang = navigator.language.toLowerCase();
+    //     if (lang.indexOf('zh') >= 0) {
+    //         // 假如浏览器语言是中文
+    //         localStorage.setItem("defaultLng", "zh")
+    //     } else {
+    //         // 假如浏览器语言是其它语言
+    //         localStorage.setItem("defaultLng", "en")
+    //     }
+    //     loadLocales()
+    // })
+    // //默认
+    // const  default_local = localStorage.getItem('locale') || localStorage.getItem("defaultLng") || 'zh'
+    // const loadLocales = () => {
+    //     intl.init({
+    //         currentLocale: default_local,
+    //         locales,
+    //     }).then(() => {
+    //         DEFAULT_PREFIX = locales
+    //         setInitDone(true)
+    //     })
+    // }
+    //
+    // const [locale, setLocale] = useState(default_local)
+    // //切换语言
+    // const handleChange = (e: any) => {
+    //     let val = e.target.value
+    //     setLocale(val)
+    //     localStorage.setItem('locale', val)
+    //     window.location.reload()
+    // }
+    // const context = {
+    //     DEFAULT_PREFIX
+    // }
     return (
-        <div  className='bg-yellow-50'>
-            <Select
-                labelId="demo-simple-select-label"
-                id="demo-simple-select"
-                value={locale}
-                label="Language"
-                onChange={handleChange}
-            >
-                <MenuItem value='zh'>中文</MenuItem>
-                <MenuItem value='en'>English</MenuItem>
-            </Select>
-            {
-                initDone && <Outlet context={context}/>
-            }
+        <div className={'max-h-screen w-full h-full'}>
+            <header className={'flex justify-between px-9 py-3 bg-gray-700'}>
+                <img src="https://readme.so/readme.svg" alt="" className={'h-12'}/>
+            </header>
+            <Outlet/>
         </div>
     );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,120 +1,82 @@
-import {Alert, AlertTitle, Button, Card, Checkbox, FormControlLabel, FormGroup, Snackbar} from "@mui/material";
-import Title from "@/components/title";
 import React, {useState} from "react";
-import {DEFAULT_DATA, DEFAULT_PREFIX, DEFAULT_LINK} from "@/constants/default";
-import SubTitle from "@/components/subtitle";
-import Detail from "@/components/detail";
-import {generateMarkdown} from "@/utils/util";
-import intl from "react-intl-universal";
-import AddOns from "@/components/add-ons";
-import MarkDown from "@/components/markdown";
+import Item from "antd/es/list/Item";
+import {DEFAULT_SECTIONS} from "@/constants/default";
+import {Button} from "antd";
+// import {DEFAULT_DATA, DEFAULT_PREFIX, DEFAULT_LINK} from "@/constants/default";
+// import SubTitle from "@/components/subtitle";
+// import Detail from "@/components/detail";
+// import {generateMarkdown} from "@/utils/util";
+// import intl from "react-intl-universal";
+// import AddOns from "@/components/add-ons";
+// import MarkDown from "@/components/markdown";
 
 export default function HomePage() {
-    const [prefix, setPrefix] = useState(DEFAULT_PREFIX)
-    const [data, setData] = useState(DEFAULT_DATA)
-    const [link, setLink] = useState(DEFAULT_LINK)
-    const [showMd, setShowMd] = useState(false)
-    const [mdContent, setMdContent] = useState()
-    const handlePrefixChange = (key: any, value: any) => {
-        const res = {...prefix}
-        // @ts-ignore
-        res[key] = value
-        setPrefix(res)
-    }
-    const handleDataChange = (key: any, value: any) => {
-        const res = {...data}
-        // @ts-ignore
-        res[key] = value
-        setData(res)
-    }
-
-    const [open, setOpen] = useState(false)
-    const handleCheckChange = (key: any, value: any) => {
-        if (!data?.githubName) {
-            setOpen(true)
-        }
-        const res = {...data}
-        // @ts-ignore
-        res[key] = value
-        setData(res)
-    }
-    const handleClose = (event?: React.SyntheticEvent | Event, reason?: string) => {
-        if (reason === 'clickaway') {
-            return;
-        }
-        setOpen(false);
-    };
-    const handleLinkChange = (key: any, value: any) => {
-        const res = {...link}
-        // @ts-ignore
-        res[key] = value
-        setLink(res)
-    }
-
-    const handleGenerate = () => {
-        setShowMd(true)
-        // @ts-ignore
-        // setMdContent(generateMarkdown(prefix, data, link))
-    }
+    // const [prefix, setPrefix] = useState(DEFAULT_PREFIX)
+    // const [data, setData] = useState(DEFAULT_DATA)
+    // const [link, setLink] = useState(DEFAULT_LINK)
+    // const [showMd, setShowMd] = useState(false)
+    // const [mdContent, setMdContent] = useState()
+    // const handlePrefixChange = (key: any, value: any) => {
+    //     const res = {...prefix}
+    //     // @ts-ignore
+    //     res[key] = value
+    //     setPrefix(res)
+    // }
+    // const handleDataChange = (key: any, value: any) => {
+    //     const res = {...data}
+    //     // @ts-ignore
+    //     res[key] = value
+    //     setData(res)
+    // }
+    //
+    // const [open, setOpen] = useState(false)
+    // const handleCheckChange = (key: any, value: any) => {
+    //     if (!data?.githubName) {
+    //         setOpen(true)
+    //     }
+    //     const res = {...data}
+    //     // @ts-ignore
+    //     res[key] = value
+    //     setData(res)
+    // }
+    // const handleClose = (event?: React.SyntheticEvent | Event, reason?: string) => {
+    //     if (reason === 'clickaway') {
+    //         return;
+    //     }
+    //     setOpen(false);
+    // };
+    // const handleLinkChange = (key: any, value: any) => {
+    //     const res = {...link}
+    //     // @ts-ignore
+    //     res[key] = value
+    //     setLink(res)
+    // }
+    //
+    // const handleGenerate = () => {
+    //     setShowMd(true)
+    //     // @ts-ignore
+    //     // setMdContent(generateMarkdown(prefix, data, link))
+    // }
 
     return (
-        <div className='p-6'>
-            {
-                showMd ?
-                    <>
-                        <div className='whitespace-pre-wrap w-full flex justify-center items-center border-2 bg-blue-50 p-6'>
-                            <MarkDown setMdContent={setMdContent} prefix={prefix} data={data} link={link}/>
-                        </div>
-                        <div className='whitespace-pre-wrap'>
-                            {
-                                mdContent
-                            }
-                        </div>
-                        <div className='w-full flex justify-center items-center mt-3'>
-                            <Button size={'large'} onClick={() => {
-                                setShowMd(false)
-                            }} variant="contained">
-                                {intl.get('btnBack')}
-                            </Button>
-                        </div>
-                    </>
-                    :
-                    <>
-                        <Title
-                            prefix={prefix}
-                            data={data}
-                            handleDataChange={handleDataChange}
-                            handlePrefixChange={handlePrefixChange}
-                        />
-                        <SubTitle
-                            data={data}
-                            handleDataChange={handleDataChange}
-                        />
-                        <Detail prefix={prefix}
-                                data={data}
-                                link={link}
-                                handleDataChange={handleDataChange}
-                                handlePrefixChange={handlePrefixChange}
-                                handleLinkChange={handleLinkChange}
-                        />
-                        <AddOns
-                            data={data}
-                            handleDataChange={handleDataChange}
-                            handleCheckChange={handleCheckChange}
-                        />
-                        <Snackbar anchorOrigin={{horizontal: 'center', vertical: 'top'}} open={open}
-                                  autoHideDuration={2000} onClose={handleClose}>
-                            <Alert onClose={handleClose} severity="warning" sx={{width: '100%'}}>
-                                Please add github username to use these others add-ons
-                            </Alert>
-                        </Snackbar>
-                        <div className='w-full flex justify-center items-center'>
-                            <Button onClick={handleGenerate} size='large' variant="contained">
-                                {intl.get('btnGen')}
-                            </Button>
-                        </div>
-                    </>
-            }
+        <div className={'p-6 flex h-full'}>
+            <div className={'w-1/5'}>
+                <div>section</div>
+                <div>
+                    {
+                        DEFAULT_SECTIONS.map((item,index)=>{
+                            return(
+                                <div key={index}>
+                                    <Button>{1}</Button>
+                                </div>
+                            )
+                        })
+                    }
+                </div>
+            </div>
+            <div className={'w-4/5'}>
+                123
+            </div>
         </div>
     );
 }
