9da1934165252eceab6e8c08499ddfb9ab8f847b
yuanli <2865437316@qq.com>
2023-04-11 17:57:00 +0800

    feat: 新增模板

--- src/locales/en-US.ts
+++ src/locales/en-US.ts
@@ -1,44 +1,325 @@
 export default {
-    'template':'Template',
-    'edit':'Edit',
-    'preview':'Preview',
-    'use':'Template used',
-    'no-use':'Template not used',
-    'btn-text':'Custom Template',
-    'modal-title':'Add Custom Template',
-    'modal-placeholder':'Template Name',
-    'copy-success':'copy success',
-    'download-success':'download success',
-    'reset-title':'Restore initial state',
-    'reset-text':'Do you want to reset the templates for all your readme files？',
-    'lang-change-title':'Language switching',
-    'lang-change-text':'Switching languages will reset the templates for all your readme files',
+    'template': 'Template',
+    'edit': 'Edit',
+    'preview': 'Preview',
+    'use': 'Template used',
+    'no-use': 'Template not used',
+    'btn-text': 'Custom Template',
+    'modal-title': 'Add Custom Template',
+    'modal-placeholder': 'Template Name',
+    'copy-success': 'copy success',
+    'download-success': 'download success',
+    'reset-title': 'Restore initial state',
+    'reset-text': 'Do you want to reset the templates for all your readme files？',
+    'lang-change-title': 'Language switching',
+    'lang-change-text': 'Switching languages will reset the templates for all your readme files',
     'title-and-description.name': 'Title and Description',
-    'title-and-description.markdown':`
+    'title-and-description.markdown': `
 # Project Title
 
 A brief description of what this project does and who it's for
 
 `,
+    'installation.name': 'Installation',
+    'installation.markdown': `
+## Installation
+
+Install my-project with npm
+
+\`\`\`bash
+  npm install my-project
+  cd my-project
+\`\`\`
+    `,
+    'logo.name': 'Logo',
+    'logo.markdown': `
+![Logo](https://dev-to-uploads.s3.amazonaws.com/uploads/articles/th5xamgrr6se0x5ro4g6.png)
+
+`,
+    'run-locally.name': 'Run Locally',
+    'run-locally.markdown': `
+## Run Locally
+
+Clone the project
+
+\`\`\`bash
+  git clone https://link-to-project
+\`\`\`
+
+Go to the project directory
+
+\`\`\`bash
+  cd my-project
+\`\`\`
+
+Install dependencies
+
+\`\`\`bash
+  npm install
+\`\`\`
+
+Start the server
+
+\`\`\`bash
+  npm run start
+\`\`\`
+
+`,
+    'screenshots.name': 'Screenshots',
+    'screenshots.markdown': `
+## Screenshots
+
+![App Screenshot](https://via.placeholder.com/468x300?text=App+Screenshot+Here)
+
+`,
+    'env-variables.name': 'Environment Variables',
+    'env-variables.markdown': `
+## Environment Variables
+
+To run this project, you will need to add the following environment variables to your .env file
+
+\`API_KEY\`
+
+\`ANOTHER_API_KEY\`
+
+`,
+    'features.name': 'Features',
+    'features.markdown': `
+## Features
+
+- Light/dark mode toggle
+- Live previews
+- Fullscreen mode
+- Cross platform
+
+`,
+    'usage-examples.name': 'Usage/Examples',
+    'usage-examples.markdown': `
+## Usage/Examples
+
+\`\`\`javascript
+import Component from 'my-project'
+
+function App() {
+  return <Component />
+}
+\`\`\`
+
+`,
+    'api.name': 'API Reference',
+    'api.markdown': `
+## API Reference
+
+#### Get all items
+
+\`\`\`http
+  GET /api/items
+\`\`\`
+
+| Parameter | Type     | Description                |
+| :-------- | :------- | :------------------------- |
+| \`api_key\` | \`string\` | **Required**. Your API key |
+
+#### Get item
+
+\`\`\`http
+  GET /api/items/$\{id}
+\`\`\`
+
+| Parameter | Type     | Description                       |
+| :-------- | :------- | :-------------------------------- |
+| \`id\`      | \`string\` | **Required**. Id of item to fetch |
+
+#### add(num1, num2)
+
+Takes two numbers and returns the sum.
+
+`,
+    'contributing.name': 'Contributing',
+    'contributing.markdown': `
+## Contributing
+
+Contributions are always welcome!
+
+See \`contributing.md\` for ways to get started.
+
+Please adhere to this project's \`code of conduct\`.
+
+`,
+    'tests.name': 'Running Tests',
+    'tests.markdown': `
+## Running Tests
+
+To run tests, run the following command
+
+\`\`\`bash
+  npm run test
+\`\`\`
+
+`,
+    'license.name': 'License',
+    'license.markdown': `
+## License
+
+[MIT](https://choosealicense.com/licenses/mit/)
+
+`,
+    'badges.name': 'Badges',
+    'badges.markdown': `
+## Badges
+
+Add badges from somewhere like: [shields.io](https://shields.io/)
+
+[![MIT License](https://img.shields.io/badge/License-MIT-green.svg)](https://choosealicense.com/licenses/mit/)
+[![GPLv3 License](https://img.shields.io/badge/License-GPL%20v3-yellow.svg)](https://opensource.org/licenses/)
+[![AGPL License](https://img.shields.io/badge/license-AGPL-blue.svg)](http://www.gnu.org/licenses/agpl-3.0)
+
+`,
+    'roadmap.name': 'Roadmap',
+    'roadmap.markdown': `
+## Roadmap
+
+- Additional browser support
+
+- Add more integrations
+
+`,
+    'authors.name': 'Authors',
+    'authors.markdown': `
+## Authors
+
+- [@octokatherine](https://www.github.com/octokatherine)
+
+`,
+    'acknowledgement.name': 'Acknowledgements',
+    'acknowledgement.markdown': `
+## Acknowledgements
+
+ - [Awesome Readme Templates](https://awesomeopensource.com/project/elangosundar/awesome-README-templates)
+ - [Awesome README](https://github.com/matiassingers/awesome-readme)
+ - [How to write a Good readme](https://bulldogjob.com/news/449-how-to-write-a-good-readme-for-your-github-project)
+
+`,
+    'support.name': 'Support',
+    'support.markdown': `
+## Support
+
+For support, email fake@fake.com or join our Slack channel.
+
+`,
+    'feedback.name': 'Feedback',
+    'feedback.markdown': `
+## Feedback
+
+If you have any feedback, please reach out to us at fake@fake.com
+
+`,
+    'related.name': 'Related',
+    'related.markdown': `
+## Related
+
+Here are some related projects
+
+[Awesome README](https://github.com/matiassingers/awesome-readme)
+
+`,
+    'demo.name': 'Demo',
+    'demo.markdown': `
+## Demo
+
+Insert gif or link to demo
+
+`,
+    'tech.name': 'Tech',
+    'tech.markdown': `
+## Tech Stack
+
+**Client:** React, Redux, TailwindCSS
+
+**Server:** Node, Express
+
+`,
+    'optimizations.name': 'Optimizations',
+    'optimizations.markdown': `
+## Optimizations
+
+What optimizations did you make in your code? E.g. refactors, performance improvements, accessibility
+
+`,
+    'lessons.name': 'Lessons',
+    'lessons.markdown': `
+## Lessons Learned
+
+What did you learn while building this project? What challenges did you face and how did you overcome them?
+
+`,
+    'faq.name': 'FAQ',
+    'faq.markdown': `
+## FAQ
+
+#### Question 1
+
+Answer 1
+
+#### Question 2
+
+Answer 2
+
+`,
+    'used-by.name': 'Used By',
+    'used-by.markdown': `
+## Used By
+
+This project is used by the following companies:
+
+- Company 1
+- Company 2
+
+`,
+    'documentation.name': 'Documentation',
+    'documentation.markdown': `
+## Documentation
+
+[Documentation](https://linktodocumentation)
+
+`,
+    'deployment.name': 'Deployment',
+    'deployment.markdown': `
+## Deployment
+
+To deploy this project run
+
+\`\`\`bash
+  npm run deploy
+\`\`\`
+
+`,
+    'appendix.name': 'Appendix',
+    'appendix.markdown': `
+## Appendix
+
+Any additional information goes here
+
+`,
     'github-intro.name': 'Github - Introduction',
-    'github-intro.markdown':`
+    'github-intro.markdown': `
 # Hi, I'm YuanLi! 👋
 
 `,
     'github-about-me.name': 'Github - About Me',
-    'github-about-me.markdown':`
+    'github-about-me.markdown': `
 ## 🚀 About Me
 I'm a front-end development engineer...
 
 `,
     'github-skills.name': 'Github - Skills',
-    'github-skills.markdown':`
+    'github-skills.markdown': `
 ## 🛠 Skills
 Javascript, HTML, CSS...
 
 `,
     'github-links.name': 'Github - Links',
-    'github-links.markdown':`
+    'github-links.markdown': `
 ## 🔗 Links
 
 [![portfolio](https://img.shields.io/badge/my_portfolio-000?style=for-the-badge&logo=ko-fi&logoColor=white)](https://katherineoelsner.com/)
@@ -48,7 +329,7 @@ Javascript, HTML, CSS...
  **[view more parameters](https://shields.io/)**
 `,
     'github-other.name': 'Github - Other',
-    'github-other.markdown':`
+    'github-other.markdown': `
 ## Other Common Github Profile Sections
 
 👩‍💻 I'm currently working on **[name](https://example.com)**
@@ -77,38 +358,50 @@ Javascript, HTML, CSS...
 
 `,
     'github-stats.name': 'Github - Stats Card',
-    'github-stats.markdown':`
-[![GitHub stats](https://github-readme-stats.vercel.app/api?username=your-github-username)](https://github.com/anuraghazra/github-readme-stats)
+    'github-stats.markdown': `
+## Replace imyuanli with your own github name
+
+[![GitHub stats](https://github-readme-stats.vercel.app/api?username=imyuanli)](https://github.com/anuraghazra/github-readme-stats)
 
  **[view more parameters](https://github.com/anuraghazra/github-readme-stats/blob/master/readme.md#github-stats-card)**
 `,
     'github-pins.name': 'Github - Extra Pins',
-    'github-pins.markdown':`
-[![Readme Card](https://github-readme-stats.vercel.app/api/pin/?username=your-github-username&repo=your-github-repo)](https://github.com/anuraghazra/github-readme-stats)
+    'github-pins.markdown': `
+## Replace imyuanli with your own github name and readme with your own warehouse
+
+[![Readme Card](https://github-readme-stats.vercel.app/api/pin/?username=imyuanli&repo=readme)](https://github.com/anuraghazra/github-readme-stats)
 
  **[view more parameters](https://github.com/anuraghazra/github-readme-stats/blob/master/readme.md#github-extra-pins)**
 `,
     'github-languages.name': 'Github - Top Languages',
-    'github-languages.markdown':`
-[![Top Langs](https://github-readme-stats.vercel.app/api/top-langs/?username=your-github-username)](https://github.com/anuraghazra/github-readme-stats)
+    'github-languages.markdown': `
+## Replace imyuanli with your own github name
+ 
+[![Top Langs](https://github-readme-stats.vercel.app/api/top-langs/?username=imyuanli)](https://github.com/anuraghazra/github-readme-stats)
 
  **[view more parameters](https://github.com/anuraghazra/github-readme-stats/blob/master/readme.md#top-languages-card)**
 `,
     'github-streak.name': 'Github - Streak Stats',
-    'github-streak.markdown':`
-[![GitHub Streak](https://github-readme-streak-stats.herokuapp.com?user=your-github-username)](https://git.io/streak-stats)
+    'github-streak.markdown': `
+## Replace imyuanli with your own github name
+
+[![GitHub Streak](https://github-readme-streak-stats.herokuapp.com?user=imyuanli)](https://git.io/streak-stats)
 
  **[view more parameters](https://github-readme-streak-stats.herokuapp.com/demo)**
 `,
     'github-visitors.name': 'Github - Views Counter',
-    'github-visitors.markdown':`
-![GitHub Profile Views Counter](https://komarev.com/ghpvc/?username=your-github-username)
+    'github-visitors.markdown': `
+## Replace imyuanli with your own github name
+    
+![GitHub Profile Views Counter](https://komarev.com/ghpvc/?username=imyuanli)
 
  **[view more parameters](https://github.com/antonkomarev/github-profile-views-counter)**
 `,
     'github-trophy.name': 'Github - Trophy',
-    'github-trophy.markdown':`
-[![trophy](https://github-profile-trophy.vercel.app/?username=your-github-username&theme=onedark)](https://github.com/ryo-ma/github-profile-trophy)
+    'github-trophy.markdown': `
+## Replace imyuanli with your own github name
+    
+[![trophy](https://github-profile-trophy.vercel.app/?username=imyuanli&theme=onedark)](https://github.com/ryo-ma/github-profile-trophy)
 
  **[view more parameters](https://github.com/ryo-ma/github-profile-trophy)**
 `,
